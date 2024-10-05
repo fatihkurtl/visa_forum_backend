cff236bb1f4b4aa5704cb57543b8b99bad8bc85d
fatihkurtl <kurtf061@gmail.com>
2024-10-05 14:56:13 +0300

    comments & reply api's added

--- threads/urls.py
+++ threads/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('<int:pk>/', views.ThreadsDetailView.as_view(), name='thread-detail'),
     path('categories/', views.ThreadsCategoriesView.as_view(), name='thread-categories'),
     path('create/', views.ThreadsCreateView.as_view(), name='thread-create'),
+    path('<int:pk>/add/comment/', views.ThreadsAddCommentView.as_view(), name='thread-add-comment'),
+    path('<int:pk>/add/reply/', views.ThreadsAddReplyView.as_view(), name='thread-add-reply'),  
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- threads/views.py
+++ threads/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from members.models import Member, Token
 from members.serializers import MemberTokenSerializer
-from .models import Category, Comments, Thread
+from .models import Category, Comments, Reply, Thread
 from .serializers import CategorySerializer, ThreadSerializer, ThreadCreateSerializer
 
 
@@ -59,3 +59,50 @@ class ThreadsCreateView(APIView):
             return Response({"token": member.token, "expires_at": member.expires_at}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ThreadsAddCommentView(APIView):
+    def post(self, request, pk, format=None):
+        member_token = request.headers.get('Authorization').split(' ')[1]
+        member = MemberTokenSerializer().check_token(member_token)            
+        thread = Thread.objects.get(pk=request.data.get('thread_id'))
+        content = request.data.get('content')
+        author = Member.objects.get(pk=member.pk)
+    
+        print(request.data)
+        print(member_token)
+        print(pk)
+        print(thread)
+        print(content)
+        print(author)
+        if member is None:
+            return Response({'message': 'Geçersiz token.'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        if not content:
+            return Response({'message': 'Eksik bilgi.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        save_comment = Comments.objects.create(thread=thread, content=content, author=author)
+        print(save_comment)
+        return Response({'message': 'Yorum kaydedildi.', 'status': 'success'}, status=status.HTTP_201_CREATED)
+    
+
+class ThreadsAddReplyView(APIView):
+    def post(self, request, pk, format=None):
+        member_token = request.headers.get('Authorization').split(' ')[1]
+        member = MemberTokenSerializer().check_token(member_token)    
+        comment = Comments.objects.get(pk=request.data.get('parent_id'))
+        print(member_token)
+        print(comment)
+        print(request.data)
+        # content = request.data.get('content')
+        # author = Member.objects.get(pk=member.pk)
+    
+        # if member is None:
+        #     return Response({'message': 'Geçersiz token.'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        # if not content:
+        #     return Response({'message': 'Eksik bilgi.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # save_reply = Reply.objects.create(thread=comment.thread, content=content, author=author, reply_to=comment)
+        # print(save_reply)
+        return Response({'message': 'Yorum kaydedildi.', 'status': 'success'}, status=status.HTTP_201_CREATED)
